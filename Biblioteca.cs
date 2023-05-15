2ded28e48464969573f5fb0c9e4d7d3a716f6f8a
Ewertonnn <132709093+Ewertonnn@users.noreply.github.com>
2023-05-15 16:14:35 -0300

    Adicionei a formula do volume do cubo

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -1,6 +1,10 @@
 class Biblioteca{
     //Escdreva a sua função alterando o que for nescessário
-    static public double NomeDaFuncao(){
-        return 100;
+    static public double volumeDocubo(double a){
+        double aresta;
+
+        aresta= a* a* a;
+
+        return aresta;
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ class Program{
     static void Main(){
         int opt = 0;
         bool repetir = true;
+        double resultado;
+        double a, aresta;
+    
+
         //Insira as variáveis do seu algoritmo aqui
 
         do{
@@ -13,12 +17,13 @@ class Program{
 
             Console.WriteLine("\tMENU");
             Console.WriteLine("1- Opção 1");
-            Console.WriteLine("2- Opção 2");
+            Console.WriteLine("2-Digite o volume do cubo");
             Console.WriteLine("3- Opção 3");
             Console.WriteLine("4- Opção 4");
             Console.WriteLine("5- Opção 5");
             Console.WriteLine("6- Opção 6");
             Console.WriteLine("99- Sair\n");
+
 
             opt = int.Parse(Console.ReadLine());
 
@@ -26,7 +31,19 @@ class Program{
             Console.Clear();
             switch (opt){
                 case 1:
-                    Console.WriteLine("Opt1");
+                    Console.WriteLine(" você escolheu a formula do volume do cubo ");
+                   
+                    Console.WriteLine("Digite o volume o cubo: ");
+
+                    a = double.Parse(Console.ReadLine());
+
+                    aresta = Biblioteca.volumeDocubo(a );
+                     
+                 break;
+
+
+                    Console.WriteLine("O Resultado do volume do cubo : (0)", resultado);
+
                     break;
                 case 2:
                     Console.WriteLine("Opt3");
